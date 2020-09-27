698519257c9ef6d0b614feee9a44ee59022a88fe
Andrii Leskiv <leshost@gmail.com>
2020-09-27 13:57:59 +0300

    Notes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React, {Fragment} from 'react';
 import {Form} from '../components/Form'
+import { Notes } from '../components/Notes';
 
 export const Home = () => {
+    const notes = new Array()
+
     return (
         <Fragment>
             <Form />
+            <hr/>
+            <Notes notes={notes} />
         </Fragment>
     )
 }
