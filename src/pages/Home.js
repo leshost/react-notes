7b79f7759fe07a5f5a580ecc00fea2aa2a219729
Andrii Leskiv <leshost@gmail.com>
2020-09-27 15:14:04 +0300

    Alert

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,14 @@ import {Form} from '../components/Form'
 import { Notes } from '../components/Notes';
 
 export const Home = () => {
-    const notes = new Array()
+    const notes = new Array(3)
+        .fill('')
+        .map((_, i) => ({
+            id: i, 
+            title: `Note ${i + 1 }`
+        }))
+
+        console.log(notes)
 
     return (
         <Fragment>
