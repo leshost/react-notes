7c170fa32daa5e1e49b6779554cb8db443ed786f
Andrii Leskiv <leshost@gmail.com>
2020-09-27 21:44:33 +0300

    AlertReducer

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
 export const Form = () => {
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        if(value.trim()) {
+            // ...
+            alert.show('Нотатка успішно створена', 'success')
+            setValue('')
+        } else {
+            alert.show('Введіть текст нотатки')
+        }
+
+        //alert.show(value, 'success')
+    }
+
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="form-group">
                 <input
                     type="text"
                     className="form-control"
                     placeholder="Введіть текст нотатки"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
                 />
             </div>
         </form>
